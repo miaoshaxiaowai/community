07e2f85939abfb2b5374a895359434f7ac0056dd
hao <1016588149@qq.com>
2020-03-04 20:28:49 +0800

    add check cookies

--- src/main/java/life/hao/community/controller/AuthorizeController.java
+++ src/main/java/life/hao/community/controller/AuthorizeController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -33,7 +35,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name ="code") String code,
                            @RequestParam(name ="state") String state,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -44,16 +46,19 @@ public class AuthorizeController {
         GithubUser githubUser =githubProvider.getUser(accessToken);
         if(githubUser != null){
             User user =new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token =UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            request.getSession().setAttribute("user",user);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
+            //登录成功，写cookie和session
         }else{
             //等罗失败埃，重新登录
+            System.out.println("失败");
             return "redirect:/";
         }
     }
